d597eccc87b47b6a74176e321d74560819ada539
theflyingmantis <rai.1@iitj.ac.in>
2017-12-16 18:29:42 +0530

    Game logics, animation and sound added

--- app/views/assets/js/home.js
+++ app/views/assets/js/home.js
@@ -1,5 +1,6 @@
 console.log('JS is working');
 window.onload = function() {
-  var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'game_stage');
+  var game = new Phaser.Game(900, 900, Phaser.AUTO, 'game_stage');
   game.state.add('menu',MenuState,true);
+  game.state.add('level_one',LevelOneState);
 };
--- app/views/assets/js/menu.js
+++ app/views/assets/js/menu.js
@@ -1,15 +1,21 @@
-var MenuState = function () {};
+var MenuState = function () {
+  this.music;//to store the sound object for the music background  
+};
 
 MenuState.prototype.preload = function() {
   this.load.image('logo','assets/img/logo.png');
   this.load.image('start','assets/img/start.jpeg');
+  this.load.audio("music","assets/audio/menu.mp3","assets/audio/menu.ogg");
+
 };
 
 MenuState.prototype.create = function() {
-  var logo =   this.add.sprite(this.world.centerX,this.world.centerY,'logo');
+  var logo =   this.add.sprite(this.world.centerX,this.world.centerY-200,'logo');
   logo.anchor.setTo(0.5,0.5);//set the anchor so that the image is centered on the stage
-  var start_btn = this.add.button(this.world.centerX,this.world.centerY+200,'start',this.OnClick,this);
+  var start_btn = this.add.button(this.world.centerX,this.world.centerY+100,'start',this.OnClick,this);
   start_btn.anchor.setTo(0.5,0.5);
+  this.music = this.add.audio("music",1,true);
+  this.music.play();
 };
 
 MenuState.prototype.update = function() {
@@ -18,5 +24,6 @@ MenuState.prototype.update = function() {
 
 
 MenuState.prototype.OnClick = function(){
-  console.log("Start Level 1 State");
+  this.state.start('level_one');
+  this.music.stop();
 };
